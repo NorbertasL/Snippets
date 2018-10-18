182482e2246924b9306da8a69832bcb84b639d9f
Red_Spark <nlapenas@gmail.com>
2018-10-18 23:03:25 +0100

    Isogram tester method

--- Snippets/src/StringTester.java
+++ Snippets/src/StringTester.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class StringTester {
 	
@@ -22,5 +23,30 @@ public class StringTester {
 		return numberOfLetter == 0 ? false : true;
        
 	}
+	
+	/**
+	 * Check if the phrase is an Isogram
+	 * @param phrase is the string to check
+	 * @return boolean
+	 */
+    public static boolean isIsogram(String phrase) {
+    	ArrayList <Character> chars = new ArrayList<>();
+		//Looping trough the string
+		for(char c : phrase.toUpperCase().toCharArray()){
+			
+			//ignoring " " and "-"
+			if(c ==' ' || c == '-') continue;
+			
+			//looping trough the chars that have been seen
+			for(char ch : chars){
+				if(c == ch) return false;
+			}
+			//we didn't see this char yet, so we add it to the list
+			chars.add(Character.toUpperCase(c));
+			
+		}
+		return true;
+        
+    }
 
 }
